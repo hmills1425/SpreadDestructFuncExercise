b191709be64f2c2bb8180cd0e7236aba2a6b6c0e
Heather Mills <heathermills1980@gmail.com>
2022-11-03 15:31:51 -0500

    Competed Exercise

---
+++ script.js
@@ -0,0 +1,78 @@
+
+
+// 1a
+const jPDinos = [
+    "Velociraptors", 
+    "Tyrannosaurus Rex",
+    "Dilophasaurus"
+];
+
+// 1b
+function seeDinos(a, ...b){
+    console.log(a);
+    console.log(b);
+}
+seeDinos(...jPDinos)
+
+// 2a
+const jPCharacters = {
+    alanGrant: "Sam Neill",
+    ellieSattler: "Laura Dern",
+    ianMalcolm: "Jeff Goldblum"
+};
+
+// 2b
+// seeCharacters = (jPCharacters) => {
+    // console.log(jPCharacters.ellieSattler);
+// }  
+
+// OR
+
+// seeCharacters = ({ellieSattler}) => {
+    // console.log(ellieSattler);
+// }
+
+// OR
+seeCharacters = ({ellieSattler}) => console.log(ellieSattler);
+
+// BONUS
+// 3a
+const jurassicParkMovies = [
+    {
+       one: "Jurassic Park",
+       two: "The Lost World: Jurassic Park",
+       three: "Jurassic Park III"
+
+    },
+    {
+        four: "Jurassic World",
+        five: "Jurassic World: Fallen Kingdom",
+        six: "Jurassic World: Dominion"
+    }
+];
+
+// 3b
+// const seeJPMovies = function ([{one, two, three,}, {four,five, six}]){
+    // console.log(one);
+    // console.log(two);
+    // console.log(three);
+    // console.log(four);
+    // console.log(five);
+    // console.log(six);
+// }
+
+// OR
+
+seeJPMovies = ([{one,two, three}, {four, five, six}]) => {
+    console.log(one);
+    console.log(two);
+    console.log(three);
+    console.log(four);
+    console.log(five);
+    console.log(six);
+}
+
+
+
+
+
